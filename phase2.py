92e475eccd1ad53bc25773e44e46f25107063cc8
ANTOmelvin001 <antomelvin1111@gmail.com>
2025-08-25 23:20:54 +0530

    complete phase

--- phase2.py
+++ phase2.py
@@ -1,3 +1,4 @@
+# LEARNING LIST
 '''Q1. Maximum Number in List
 
 Write a program that takes a list of numbers and prints the largest number without using max().
@@ -69,3 +70,65 @@ for i in d:
     if i > second_biggest and i != biggest:
         second_biggest=i
 print(second_biggest)
+
+
+#LEARNING TUPLE
+cities=("Erode","coimbator","trichy","chennai")
+print(cities[0],cities[2],cities[-1])
+print(len(cities))
+#2
+num=[1,2,3]
+tuple_sum=0
+for i in num:
+    tuple_sum=tuple_sum+i
+print(tuple_sum)
+print(max(num))
+#3
+cities=("Erode","coimbator","trichy","chennai")
+if "chennai" in cities:
+    print("TURE")
+else:
+    print("FALES")
+
+#PRATICE CODE👌
+"""Q1. Swap Two Numbers using Tuple
+
+Write a program to swap two numbers without using a third variable."""
+tuple_a=3
+tuple_b=4
+tuple_a,tuple_b=tuple_b,tuple_a
+print(tuple_a,tuple_b)
+
+'''Q2. Count Occurrences in Tuple
+Write a program to count how many times a number appears in a tuple.'''
+Occurrences=[1,2,3,4,5,1,3,0,2]
+target=int(input("enter target:"))
+
+count=0
+for i in Occurrences:
+    if i==target:
+        count +=1
+print(count)
+# another way
+count=Occurrences.count(target)
+print("the occurrence is :",count)
+
+'''Find Min and Max in Tuple
+Write a program to find the minimum and maximum number in a tuple without using min() or max().'''
+# Tuple of numbers
+numbers = (5, 2, 9, 1, 7, 3)
+
+# Assume the first number is both min and max
+minimum = numbers[0]
+maximum = numbers[0]
+
+# Loop through the tuple
+for num in numbers:
+    if num < minimum:
+        minimum = num
+    if num > maximum:
+        maximum = num
+
+print(f"Minimum number is: {minimum}")
+print(f"Maximum number is: {maximum}")
+
